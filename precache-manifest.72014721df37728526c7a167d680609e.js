4c4427d83159f71516f7ebbf0e3291c45674e452
Alexandr <Akeley1994@gmail.com>
2020-02-09 14:29:32 +0200

    Updates

---
+++ precache-manifest.72014721df37728526c7a167d680609e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ff77037a83f96a8b41996c9cfcc444",
+    "url": "/task-react/index.html"
+  },
+  {
+    "revision": "f62a8aed89430d8503c1",
+    "url": "/task-react/static/css/main.8f8cd9de.chunk.css"
+  },
+  {
+    "revision": "db0472056509a5568b39",
+    "url": "/task-react/static/js/2.07c9ceba.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/task-react/static/js/2.07c9ceba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62a8aed89430d8503c1",
+    "url": "/task-react/static/js/main.825773c1.chunk.js"
+  },
+  {
+    "revision": "31f425dd2a99bce9f882",
+    "url": "/task-react/static/js/runtime-main.677b7a80.js"
+  }
+]);
